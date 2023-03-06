b3d91d48257171effbd809acb241b64148a7206a
Hamid <hamid.iqbal2001@gmail.com>
2023-03-06 22:13:27 +0000

    basic playback functionality implemented

--- src/sections/Buttons.tsx
+++ src/sections/Buttons.tsx
@@ -1,21 +1,81 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableHighlight, Pressable, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Image, Pressable } from "react-native";
+import { Audio } from 'expo-av';
+
+
 
 export const Buttons = () => {
+    const [audio, setAudio] = useState<Audio.Sound>()
+
+    const getAudio = async () => {
+        // load the audio from online source - probs the best method tbh
+        const { sound } = await Audio.Sound.createAsync({ uri: 'https://github.com/hamid-hai/nook_mobile/raw/main/src/assets/audio/1am.mp3' })
+        console.log('Creating async')
+
+        // load the audio from assets
+        // const { sound } = await Audio.Sound.createAsync(require('./assets/1am.mp3'))
+
+        // sets the audio state to the newly retrieved sound
+        setAudio(sound)
+        console.log('Setting Audio')
+
+        // plays the audio and sets to loop
+        sound.setIsLoopingAsync(true)
+        console.log('Set Looping to True')
+        await sound.playAsync()
+        console.log('Waiting for playAudio function')
+    }
+
+    const playAudio = async () => {
+        await audio?.playAsync()
+        console.log('Playing audio from playAudio')
+    }
+
+    const pauseAudio = async () => {
+        await audio?.pauseAsync()
+        console.log('Pausing audio')
+    }
+
+    // stops audio from playing and sets state back to undefined
+    const stopAudio = async () => {
+        await audio?.stopAsync()
+        setAudio(undefined)
+        console.log('Audio stopped from stopAudio')
+    }
+
+    useEffect(() => {
+        return () => {
+            console.log('unloading sound')
+            audio ? audio.unloadAsync() : undefined
+        }
+
+    }, [audio])
+
     return (
         <View style={styles.row}>
 
+            {/* Get audio should be tied to picker to automatically download files depending on the chosen game */}
             <Pressable
-                onPress={() => Alert.alert('Play Button Pressed')}>
+                onPress={getAudio}>
+                <Image style={styles.image} source={require('../assets/icons/download-icon.png')} />
+            </Pressable>
+
+            {/* Play and Pause can be combined into one button, these control the file locally without having to re-download the file each time */}
+            <Pressable
+                onPress={playAudio}>
                 <Image style={styles.image} source={require('../assets/icons/play-icon.png')} />
             </Pressable>
 
             <Pressable
-                onPress={() => Alert.alert('Pause Button Pressed')}>
+                onPress={pauseAudio}>
                 <Image style={styles.image} source={require('../assets/icons/pause-icon.png')} />
             </Pressable>
 
-            {/* <Image source={require('/Users/hamid/Documents/Projects/nook_mobile_one/nook_mobile/assets/icons/pause-icon.png')} /> */}
+            {/* Stop audio isn't exactly needed at this stage, but kills off the audio functionality and dumps the file. */}
+            <Pressable
+                onPress={stopAudio}>
+                <Image style={styles.image} source={require('../assets/icons/stop-icon.png')} />
+            </Pressable>
         </View>
     )
 }
@@ -34,3 +94,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 })
+
+function componentDidMount() {
+    throw new Error("Function not implemented.");
+}
