4b55290af6abd477a5116db0f8c8d4b062024609
Hamid <hamid.iqbal2001@gmail.com>
2023-03-06 16:12:59 +0000

    Code cleanup

--- src/sections/Controls.tsx
+++ src/sections/Controls.tsx
@@ -2,36 +2,26 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const Controls = () => {
-        return(
-            <View style={styles.box}>
-                    <Text style={styles.inner}>
-                        Controls
-                    </Text>
-            </View>
-        )
+    return (
+        <View style={styles.column}>
+            <Text style={styles.inner}>
+                Controls
+            </Text>
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: '85%',
-        padding: 5,
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-    },
-    box: {
-        width: '100%',
-        height: '25%',
-        padding: 5,
+    column: {
+        flexDirection: 'column',
+        alignItems: 'center',
+
     },
     inner: {
-        flex: 1,
-        backgroundColor: '#9a87ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#e2f7ec', 
+        color: '#e2f7ec',
         fontSize: 30,
         fontWeight: 'bold',
         textAlign: 'center',
-    }
+    },
 })
