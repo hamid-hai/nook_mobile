f7c669d58f75c853a8497ffbe9b69fe4170f034f
Hamid <hamid.iqbal2001@gmail.com>
2023-03-08 17:32:24 +0000

    RNPicker is now linked to choice of game

--- src/sections/GamePicker.tsx
+++ src/sections/GamePicker.tsx
@@ -35,9 +35,9 @@ export const GamePicker = () => {
         },
     ];
 
-    const getAudio = async () => {
+    const getAudio = async (value: Text) => {
         console.log(dateTimeHours + 'Hours')
-        const { sound } = await Audio.Sound.createAsync({ uri: 'https://github.com/hamid-hai/nook_mobile/blob/main/src/assets/audio/WWCF/' + dateTimeHours + '.mp3?raw=true' })
+        const { sound } = await Audio.Sound.createAsync({ uri: 'https://github.com/hamid-hai/nook_mobile/blob/main/src/assets/audio/' + value + '/' + dateTimeHours + '.mp3?raw=true' })
         console.log('Creating async')
 
         // load the audio from assets
@@ -93,7 +93,7 @@ export const GamePicker = () => {
                     color: '#9EA0A4'
                 }}
                 textInputProps={styles.inner}
-                onValueChange={getAudio}
+                onValueChange={(value) => getAudio(value)}
                 items={animalCrossingGames}
             />
 
